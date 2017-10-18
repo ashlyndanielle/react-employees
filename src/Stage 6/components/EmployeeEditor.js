cd8deba45945448d172a729878f69ef760920d65
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-18 15:07:04 -0600

    completed stage 6

--- src/Stage 6/components/EmployeeEditor.js
+++ src/Stage 6/components/EmployeeEditor.js
@@ -10,13 +10,36 @@ class EmployeeEditor extends Component {
     };
   }
 
-  // componentWillReceiveProps
+  componentWillReceiveProps(props) {
+    this.setState({
+      employee: Object.assign({}, props.selected),
+      originalEmployee: props.selected,
+      notModified: true
+    })
+  }
 
-  // handleChange
+  handleChange = (prop, value) => {
+    if ( this.state.notModified ) {
+      this.setState({ notModified: false })
+    }
+    let employeeCopy = Object.assign({}, this.state.employee);
+    employeeCopy[prop] = value;
+    this.setState({
+      employee: employeeCopy
+    })
+  }
 
-  // save
+  save = () => {
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.setState({ notModified: true });
+    this.props.refreshList();
+  }
 
-  // cancel
+  cancel = () => {
+    this.setState({ employee: this.state.originalEmployee })
+  }
   
   render() {
     return (
